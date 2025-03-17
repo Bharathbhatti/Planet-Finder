876e4286e65f0c2cd0f766844a4b9103e7a9796d
Bharathbhatti <bharathbn2000@gmail.com>
2025-03-17 21:11:38 +0530

    Fetch planets from search Text

--- src/app/components/Planet.tsx
+++ src/app/components/Planet.tsx
@@ -7,7 +7,9 @@ import {
   fetchPlanets,
   fetchShapes,
   fetchSizes,
+  getFilteredPlanets,
 } from "../API/getdata";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const { Option } = Select;
 
@@ -33,6 +35,7 @@ interface Size {
   name: string;
 }
 interface Filters {
+  q: string;
   color: Color["id"][];
   shape: Shape["id"][];
   size: Size["id"][];
@@ -41,6 +44,7 @@ interface Filters {
 const PlanetSearch = () => {
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [filters, setFilters] = useState<Filters>({
+    q: "",
     color: [],
     shape: [],
     size: [],
@@ -50,7 +54,29 @@ const PlanetSearch = () => {
   const [colors, setColors] = useState<Color[]>([]);
   const [shapes, setShapes] = useState<Shape[]>([]);
 
-  const handleFilterChange = (key: keyof Filters, value: string[]) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const setLocationByFilterProps = () => {
+    const queryParams = new URLSearchParams();
+    if (filters.q.length > 0) {
+      queryParams.set("q", filters.q);
+    }
+    if (filters.color.length > 0) {
+      queryParams.set("colors", filters.color.join(","));
+    }
+    if (filters.shape.length > 0) {
+      queryParams.set("shapes", filters.shape.join(","));
+    }
+    if (filters.size.length > 0) {
+      queryParams.set("sizes", filters.size.join(","));
+    }
+    router.replace("?" + queryParams.toString(), {
+      scroll: false,
+      shallow: true,
+    } as any);
+  };
+
+  const handleFilterChange = (key: keyof Filters, value: string[] | string) => {
     setFilters((prev) => ({ ...prev, [key]: value as Filters[typeof key] }));
     // console.log(filters);
   };
@@ -70,14 +96,56 @@ const PlanetSearch = () => {
     console.log(planets);
   }, []);
 
+  useEffect(() => {
+    setLocationByFilterProps();
+    const updatePlanets = async () => {
+      const updatedPlanets = await getFilteredPlanets(filters);
+      setPlanets(updatedPlanets);
+    };
+    updatePlanets();
+  }, [filters]);
+
+  useEffect(() => {
+    setFilterFromQueryParams();
+  }, [searchParams]);
+
+  const setFilterFromQueryParams = () => {
+    if (typeof window !== "undefined") {
+      const queryParams = new URLSearchParams(window.location.search);
+      const queryColors = queryParams.get("colors");
+      const queryShapes = queryParams.get("shapes");
+      const querySizes = queryParams.get("sizes");
+      const tempFilter = {
+        q: searchTerm,
+        color: queryColors ? queryColors.split(",") : [],
+        shape: queryShapes ? queryShapes.split(",") : [],
+        size: querySizes ? querySizes.split(",") : [],
+      };
+      setFilters(tempFilter);
+    }
+  };
+
+  const ifPlanetMatches = (planet: Planet) => {
+    if (filters.color.length > 0 && !filters.color.includes(planet.color)) {
+      return false;
+    }
+    if (filters.shape.length > 0 && !filters.shape.includes(planet.shape)) {
+      return false;
+    }
+    if (filters.size.length > 0 && !filters.size.includes(planet.size)) {
+      return false;
+    }
+    return true;
+  };
+
   return (
     <div className="p-6 max-w-4xl mx-auto space-y-4">
       <Input.Search
         placeholder="Search planets..."
         value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.id)}
+        onChange={(e) => setSearchTerm(e.target.value)}
         onSearch={() => {}}
-        onPressEnter={() => {}}
+        onPressEnter={(e) => handleFilterChange("q", e.currentTarget.value)}
       />
       <div className="grid grid-cols-3 gap-4">
         <Select
@@ -85,6 +153,7 @@ const PlanetSearch = () => {
           placeholder="Filter by color"
           className="w-full"
           onChange={(value) => handleFilterChange("color", value)}
+          value={filters.color}
         >
           {colors.map((color: Color) => (
             <Option key={color.id} value={color.id}>
@@ -97,6 +166,7 @@ const PlanetSearch = () => {
           placeholder="Filter by shape"
           className="w-full"
           onChange={(value) => handleFilterChange("shape", value)}
+          value={filters.shape}
         >
           {shapes.map((shape: Shape) => (
             <Option key={shape.id} value={shape.id}>
@@ -109,6 +179,7 @@ const PlanetSearch = () => {
           placeholder="Filter by size"
           className="w-full"
           onChange={(value) => handleFilterChange("size", value)}
+          value={filters.size}
         >
           {sizes.map((size: Size) => (
             <Option key={size.id} value={size.id}>
@@ -118,22 +189,25 @@ const PlanetSearch = () => {
         </Select>
       </div>
       <div className="space-y-4">
-        {planets.map((planet: Planet) => (
-          <Card key={planet.name}>
-            <h2>{planet.name}</h2>
-            <p>
-              Color:{" "}
-              {colors.find((color) => color.id === planet.color)?.name ?? ""}
-            </p>
-            <p>
-              Shape:{" "}
-              {shapes.find((shape) => shape.id === planet.shape)?.name ?? ""}
-            </p>
-            <p>
-              Size: {sizes.find((size) => size.id === planet.size)?.name ?? ""}
-            </p>
-          </Card>
-        ))}
+        {planets.map((planet: Planet) =>
+          ifPlanetMatches(planet) ? (
+            <Card key={planet.name}>
+              <h2>{planet.name}</h2>
+              <p>
+                Color:{" "}
+                {colors.find((color) => color.id === planet.color)?.name ?? ""}
+              </p>
+              <p>
+                Shape:{" "}
+                {shapes.find((shape) => shape.id === planet.shape)?.name ?? ""}
+              </p>
+              <p>
+                Size:{" "}
+                {sizes.find((size) => size.id === planet.size)?.name ?? ""}
+              </p>
+            </Card>
+          ) : null
+        )}
       </div>
     </div>
   );
